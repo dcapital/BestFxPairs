bf4b17016d83967bee09782455d3f677a8c290e1
Yoana Dimitrova <yoanadim@hotmail.com>
2018-02-05 19:16:46 +0000

    Initial commit including rest api wrapper.
    Initial project structure for a .NET core console app with imported rest API wrapper and a unit test project.

---
+++ BestFxPairs/CSharpLibAPISample/README.md
@@ -0,0 +1,6 @@
+CSharpLibAPISample
+==================
+
+Sample C# library that provides easy access to the new OANDA rest api
+
+Note that this library is only a subset of the full api
---
+++ BestFxPairs/CSharpLibAPISample/RestAPI/DataTypes/Order.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApi.DataTypes
+{
+    public class Order
+    {
+        [DisplayName("ID")]
+        public long id { get; set; }
+        [DisplayName("Type")]
+        public string type { get; set; }
+        [DisplayName("Side")]
+        public string side { get; set; }
+        [DisplayName("Instrument")]
+        public string instrument { get; set; }
+        [DisplayName("Units")]
+        public int units { get; set; }
+        [DisplayName("Time")]
+        public DateTime time { get; set; }
+        [DisplayName("Price")]
+        public double price { get; set; }
+        [DisplayName("Take Profit")]
+        public double takeProfit { get; set; }
+        [DisplayName("Expiry")]
+        public DateTime expiry { get; set; }
+        [DisplayName("Upper Bound")]
+        public double upperBound { get; set; }
+        [DisplayName("Lower Bound")]
+        public double lowerBound { get; set; }
+        [DisplayName("Trailing Stop")]
+        public int trailingStop { get; set; }
+    }
+}
