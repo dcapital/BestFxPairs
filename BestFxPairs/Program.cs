c83461b337462d5ecd726186a90576b1e1f7222e
Yoana Dimitrova <yoana.dimitrova@riskfirst.bg>
2018-02-05 19:05:40 +0000

    Add project files.

---
+++ BestFxPairs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestFxPairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
